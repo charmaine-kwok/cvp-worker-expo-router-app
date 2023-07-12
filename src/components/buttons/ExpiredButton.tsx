f414d0f0ad326d0569e7b65040403fa9f0592849
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-12 12:16:31 +0800

    add translations

--- src/components/buttons/ExpiredButton.tsx
+++ src/components/buttons/ExpiredButton.tsx
@@ -1,18 +1,21 @@
 import { View } from "react-native-ui-lib";
 import { useAtomValue } from "jotai";
 import { useRouter } from "expo-router";
+import { useTranslation } from "react-i18next";
 
 import CustomButton from "./CustomButton";
 import { DarkThemeAtom } from "~atoms/darkTheme";
 
 const ExpiredButton: React.FC = () => {
+  const { t } = useTranslation();
+
   const router = useRouter();
   const isDarkTheme = useAtomValue(DarkThemeAtom);
 
   return (
     <View className="absolute bottom-0 w-full mb-4 flex items-center px-4">
       <CustomButton
-        text="Expired Credentials"
+        text={t("Expired Credentials")}
         flexDir="column"
         fgColor={isDarkTheme ? "#000000" : "#ffffff"}
         widthPerct="100%"
