c448c77061f30eeb499b8f0bbad1db621c42c230
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-08-11 14:55:22 +0800

    update sign in screen

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,17 +1,18 @@
-import { Text, View, Image, Alert } from "react-native";
-import { useState } from "react";
+import { Image, Alert } from "react-native";
+import { View, Text, Button } from "react-native-ui-lib";
+import Dash from "react-native-dash";
 import { Stack } from "expo-router";
 import { FontAwesome, Entypo } from "@expo/vector-icons";
 import { useTranslation } from "react-i18next";
 import { useRouter } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useSetAtom } from "jotai";
+import { useForm } from "react-hook-form";
 
 import { usernameAtom } from "~atoms/username";
 import LanguagePicker from "~components/LanguagePicker";
 import DismissKeyboard from "~components/DismissKeyboard";
-import CustomInput from "~components/CustomInput";
-import CustomButton from "~components/buttons/CustomButton";
+import CustomInput from "~components/input/CustomInput";
 
 import { accessTokenAtom } from "~atoms/accessToken";
 
@@ -20,16 +21,21 @@ export default function SignIn() {
 
   const router = useRouter();
 
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
   const setUsernameData = useSetAtom(usernameAtom);
   const setAccessToken = useSetAtom(accessTokenAtom);
 
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-
-  const loginHandler: (username: string, password: string) => void = (
-    username,
-    password,
-  ) => {
+  const loginHandler = async ({ username, password }) => {
     fetch("http://192.168.1.12:8081/auth/", {
       method: "POST",
       headers: {
@@ -76,39 +82,55 @@ export default function SignIn() {
             resizeMode="contain"
             source={require("../../assets/icon/icons8.png")}
           ></Image>
-          <Text className="text-2xl mb-4">{`${t("Hello")}`}</Text>
-          <Text className="mb-2">{`${t("WelcomeText")}`}</Text>
-          <CustomInput
-            placeholder={`${t("UsernamePlaceholder")}`}
-            setValue={setUsername}
-            value={username}
-            icon={<FontAwesome name="user" size={24} color="darkgrey" />}
-            // rules={{ required: "Username is required" }}
-          />
-
-          <CustomInput
-            placeholder={`${t("PasswordPlaceholder")}`}
-            setValue={setPassword}
-            value={password}
-            icon={<Entypo name="lock" size={24} color="darkgrey" />}
-            password={true}
-            // rules={{ required: "Username is required" }}
-          />
-
-          <CustomButton
-            addStyle={{ width: "70%", flexDirection: "column" }}
-            widthPerct={80}
-            text={`${t("SignIn")}`}
-            onPress={() => loginHandler(username, password)}
-            bgColor="black"
-            flexDir="column"
-          />
-
-          <Text className="text-black my-2 sm:my-6">{`${t(
-            "ForgotPassword",
+          <Text black className="text-2xl mb-4">{`${t("Hello")}`}</Text>
+          <Text black className="mb-4">{`${t(
+            "Please Login to Your Account",
           )}`}</Text>
 
-          <Text className="text-black my-2 sm:my-4">{`${t("SignUp")}`}</Text>
+          <CustomInput
+            placeholder={`${t("Username")}`}
+            icon={<FontAwesome name="user" size={24} color="darkgrey" />}
+            password={false}
+            name="username"
+            control={control}
+            errors={errors}
+          />
+          <CustomInput
+            placeholder={`${t("Password")}`}
+            icon={<Entypo name="lock" size={24} color="darkgrey" />}
+            password={true}
+            name="password"
+            control={control}
+            errors={errors}
+          />
+
+          <Button
+            bg-black
+            onPress={handleSubmit(loginHandler)}
+            className="w-[70%] my-2"
+          >
+            <Text white className="font-bold">{`${t("SignIn")}`}</Text>
+          </Button>
+          <View className="flex flex-row items-center my-4 space-x-8 ">
+            <Dash
+              style={{ width: "35%" }}
+              dashGap={10}
+              dashLength={10}
+              dashThickness={1}
+            />
+            <Text black>{t("Or")}</Text>
+            <Dash
+              style={{ width: "35%" }}
+              dashGap={10}
+              dashLength={10}
+              dashThickness={1}
+            />
+          </View>
+          <Button bg-green10 className="w-[70%] my-2">
+            <Text white className="font-bold">{`${t(
+              "Login by iAM Smart",
+            )}`}</Text>
+          </Button>
           <LanguagePicker />
         </View>
       </DismissKeyboard>
