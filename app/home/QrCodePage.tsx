3727206c1a377828d360d7a68e78b49b28c4ec49
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-09-07 16:24:30 +0800

    update QrCodePage

--- app/home/QrCodePage.tsx
+++ app/home/QrCodePage.tsx
@@ -1,18 +1,17 @@
-import { View, Text } from "react-native-ui-lib";
+import { View } from "react-native-ui-lib";
 import { useSearchParams } from "expo-router";
 
 import QrCode from "~components/qrCode/qrCode";
 
 export default function QrCodePage() {
-  const t = useSearchParams();
-  const data = t.data as string;
+  const params = useSearchParams();
+  const data = params.data as string;
+  const workerId = params.workerId as string;
 
-  // list of UUIDs
-  console.log(JSON.parse(data), "parsed data");
   const listOfUUID: string[] = JSON.parse(data);
   return (
     <View bg-screenBG className="flex items-center justify-center py-4 h-full">
-      <QrCode UUIDs={listOfUUID} withIcon={false} />
+      <QrCode UUIDs={listOfUUID} workerId={workerId} withIcon={false} />
     </View>
   );
 }
