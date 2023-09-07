a3c03363ee1e037c98c01930c7e9991182f3fda4
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-09-07 16:30:11 +0800

    update Settings page

--- app/home/settings/Settings.tsx
+++ app/home/settings/Settings.tsx
@@ -1,13 +1,12 @@
+import { useState } from "react";
 import { Pressable, FlatList, View } from "react-native";
 import { useTranslation } from "react-i18next";
-import { useRouter } from "expo-router";
-import { openBrowserAsync } from "expo-web-browser";
-import { AntDesign } from "@expo/vector-icons";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Text } from "react-native-ui-lib";
+import { useRouter } from "expo-router";
+import { AntDesign } from "@expo/vector-icons";
 import { useAtomValue } from "jotai";
-import { useState } from "react";
 
+import { removeAccessToken } from "app/(auth)/sign-in";
 import FontSizeModal from "~components/modal/FontSizeModal";
 import { fontSizeAtom } from "~atoms/fontSize";
 
@@ -19,38 +18,30 @@ export default function Settings() {
 
   const fontSizeData = useAtomValue(fontSizeAtom);
 
-  const _handlePressButtonAsync = async () => {
-    let result = await openBrowserAsync("https://www.cic.hk/chi/");
-  };
-
   const settingsList: { name: string; onPress: () => any }[] = [
     {
-      name: `${t("Language")}`,
+      name: t("Language"),
       onPress: () => {
         router.push("/home/settings/language");
       },
     },
     {
-      name: `${t("FontSize")}`,
+      name: t("FontSize"),
       onPress: () => {
         setIsVisible(true);
       },
     },
     {
-      name: `${t("DarkMode")}`,
+      name: t("DarkMode"),
       onPress: () => {
         router.push("/home/settings/theme");
       },
     },
-    // {
-    //   name: `${t("AboutCIC")}`,
-    //   onPress: _handlePressButtonAsync,
-    // },
     {
-      name: `${t("SignOut")}`,
+      name: t("SignOut"),
 
       onPress: () => {
-        AsyncStorage.removeItem("accessToken");
+        removeAccessToken("accessToken");
         router.replace("/(auth)/sign-in");
       },
     },
@@ -64,9 +55,9 @@ export default function Settings() {
         data={settingsList}
         ListFooterComponent={
           <View className="opacity-60 justify-between flex-row border-b-2 py-4 mx-2 border-slate-300 ">
-            <Text textColor className={`ml-4 text-${fontSizeData + 1}xl`}>{`${t(
-              "Build",
-            )}`}</Text>
+            <Text textColor className={`ml-4 text-${fontSizeData + 1}xl`}>
+              {t("Build")}
+            </Text>
             <Text textColor className={`mr-4 text-${fontSizeData + 1}xl `}>
               v 1.0.0
             </Text>
