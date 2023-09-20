51aee1cbb3f902ec13f9a18b68138e473d222865
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-09-20 14:24:41 +0800

    update BarCodeScanner component

--- src/components/barCodeScanner/BarCodeScanner.tsx
+++ src/components/barCodeScanner/BarCodeScanner.tsx
@@ -11,16 +11,22 @@ import { useTranslation } from "react-i18next";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import BarcodeMask from "react-native-barcode-mask";
 import { useAtomValue } from "jotai";
+import { useRouter } from "expo-router";
 
-import { validateScannedCert } from "~functions/helper/ajv";
+import { usernameAtom } from "~atoms/username";
+import { itemProps } from "~functions/api/credential/getCredentialList";
+import {
+  validateScannedFields,
+  validateScannedUUIDs,
+} from "~functions/helper/ajv";
 import getTime from "~functions/getTime";
-import getCertDetails from "~functions/api/credential/getCredentialDetails";
-import { accessTokenAtom } from "~atoms/accessToken";
-
+import { credentialsAtom } from "~atoms/credentials";
 const { width } = Dimensions.get("window");
 
 export default function BarCodeScan() {
-  const accessToken = useAtomValue(accessTokenAtom);
+  const router = useRouter();
+  const credentials = useAtomValue(credentialsAtom);
+  const username = useAtomValue(usernameAtom);
 
   const { t } = useTranslation();
   const [hasPermission, setHasPermission] = useState<boolean | null>(null);
@@ -34,76 +40,99 @@ export default function BarCodeScan() {
     getBarCodeScannerPermissions();
   }, []);
 
-  const handleBarCodeScanned = async ({
-    type,
-    data,
-  }: {
-    type: string;
-    data: string;
-  }) => {
+  const handleBarCodeScanned = async ({ data }: { data: string }) => {
     setScanned(true);
+
     const timeObj = getTime();
     const currentTime = timeObj.currentTime;
 
-    let parsedData: any;
-    let isValidJson: boolean = false;
-    try {
-      parsedData = JSON.parse(data);
-      isValidJson = true;
-    } catch (error) {
-      isValidJson = false;
-    }
-    console.log("Scanned data:", data);
-    console.log("parsedData", parsedData);
+    let scannedData: {
+      date: string;
+      UUIDs?: string[];
+      selectedFields?: string[];
+      workerId?: string;
+      timeStamp: number;
+    };
 
     // QR code checking
-    // Check if the scanned QR code has expired (i.e. produced more than 1 minute ago)
-    // console.log((currentTime - parsedData.timeStamp) / 1000);
-    if (!isValidJson) {
+    try {
+      scannedData = JSON.parse(data);
+    } catch (error) {
+      console.log(error);
       console.log("Scanned data is not a JSON object");
       Alert.alert("Error", "Invalid data format", [
         { text: "OK", onPress: () => console.log("OK Pressed") },
       ]);
-    } else if (validateScannedCert(parsedData)) {
-      const timeStamp = parsedData.timeStamp as number;
-      if (currentTime - timeStamp > 60000) {
-        Alert.alert("Error", "The QR Code has expired", [
+    }
+    console.log("Scanned data:", scannedData);
+
+    if (username !== "admin") {
+      console.log("sth like that");
+
+      if (validateScannedFields(scannedData)) {
+        const timeStamp = scannedData.timeStamp as number;
+        // Check if the scanned QR code has expired (i.e. produced more than 1 minute ago)
+        if (currentTime - timeStamp > 60000) {
+          Alert.alert("Error", "The QR Code has expired", [
+            { text: "OK", onPress: () => console.log("OK Pressed") },
+          ]);
+        }
+
+        const filteredCreds = credentials.filter((credential: itemProps) => {
+          return (
+            scannedData.selectedFields.includes(credential.issuer) &&
+            scannedData.selectedFields.includes(credential.credentialType)
+          );
+        });
+
+        router.push({
+          pathname: "home/QrCodePage",
+          params: {
+            data: JSON.stringify(filteredCreds.map((item) => item.UUID)),
+            workerId: username,
+          },
+        });
+      } else {
+        Alert.alert("Error", "Invalid data format", [
           { text: "OK", onPress: () => console.log("OK Pressed") },
         ]);
       }
-      const fetchData = async () => {
-        try {
-          const data = await getCertDetails(parsedData.UUID, accessToken);
-          if (data) {
-            Alert.alert(
-              `${t("ScannedResult")}`,
-              `UUID: ${data.UUID}\n Credential Type: ${data.credential_type}\n Start Date: ${data.start_date}\n End Date: ${data.end_date}\n Is Valid: ${data.is_valid}\n Issuer: ${data.issuer}`,
-              [{ text: "OK", onPress: () => console.log("OK Pressed") }],
-            );
-          }
-        } catch (e) {
-          console.log("error:", e);
-        }
-      };
-      fetchData();
-      console.log("scanned", data);
     } else {
-      Alert.alert("Error", "Invalid data format", [
-        { text: "OK", onPress: () => console.log("OK Pressed") },
-      ]);
+      console.log("sth like that");
+      if (validateScannedUUIDs(scannedData)) {
+        const timeStamp = scannedData.timeStamp as number;
+        // Check if the scanned QR code has expired (i.e. produced more than 1 minute ago)
+        if (currentTime - timeStamp > 60000) {
+          Alert.alert("Error", "The QR Code has expired", [
+            { text: "OK", onPress: () => console.log("OK Pressed") },
+          ]);
+        }
+
+        console.log("scanned", scannedData);
+
+        router.push({
+          pathname: "home/ScannedResultsPage",
+          params: {
+            data: data,
+          },
+        });
+      } else {
+        Alert.alert("Error", "Invalid data format", [
+          { text: "OK", onPress: () => console.log("OK Pressed") },
+        ]);
+      }
     }
   };
 
-  // TODO: translation
   if (hasPermission === null) {
     return (
       <View className="justify-center items-center">
-        <Text>Requesting for camera permission</Text>
+        <Text>{t("Requesting for camera permission")}</Text>
       </View>
     );
   }
   if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+    return <Text>{t("No access to camera")}</Text>;
   }
 
   return (
