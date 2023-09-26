efbf1fb9d0baeae1905cfc667d5fb9f5ee6651dd
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-09-26 15:33:00 +0800

    update and rename to WorkerQrCodePage

--- app/home/WorkerQrCodePage.tsx
+++ app/home/WorkerQrCodePage.tsx
@@ -3,19 +3,21 @@ import { useSearchParams } from "expo-router";
 
 import QrCode, { TypeUUIDsQRCode } from "~components/qrCode/qrCode";
 
-export default function QrCodePage() {
+export default function WorkerQrCodePage() {
   const params = useSearchParams();
   const data = params.data as string;
   const workerId = params.workerId as string;
 
   const listOfUUID: string[] = JSON.parse(data);
   return (
-    <View bg-screenBG className="flex items-center justify-center py-4 h-full">
-      <QrCode<TypeUUIDsQRCode>
-        UUIDs={listOfUUID}
-        workerId={workerId}
-        withIcon={false}
-      />
+    <View className="flex items-center justify-center py-4 h-full bg-green-400">
+      <View bg-screenBG className="w-[80%] p-10 rounded-3xl flex items-center">
+        <QrCode<TypeUUIDsQRCode>
+          UUIDs={listOfUUID}
+          workerId={workerId}
+          withIcon={true}
+        />
+      </View>
     </View>
   );
 }
