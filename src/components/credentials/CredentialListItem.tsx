99b735dc2976f17257070ef367af1e9ebb17e7f6
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-09-26 15:30:23 +0800

    update CredentialListItem

--- src/components/credentials/CredentialListItem.tsx
+++ src/components/credentials/CredentialListItem.tsx
@@ -1,9 +1,7 @@
 import { Text, View } from "react-native-ui-lib";
 import { useAtomValue } from "jotai";
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
-import MoreInfo from "./MoreInfo";
 import { fontSizeAtom } from "~atoms/fontSize";
 import { itemProps } from "~functions/api/credential/getCredentialList";
 
@@ -15,22 +13,32 @@ const CredentialListItem: React.FC<CredentialListItemProps> = ({ item }) => {
   const { t } = useTranslation();
 
   const fontSizeData = useAtomValue(fontSizeAtom);
-  const [isHintVisible, setIsHintVisible] = useState(false);
 
   return (
     <View className="relative flex flex-row items-center border-t px-2 py-6">
-      <Text className={`text-${fontSizeData}xl text-black`}>{`${t(
-        "Credential Type",
-      )}: ${t(`${item.credentialType}`)} `}</Text>
+      <View className="flex flex-row justify-between mb-4 w-full">
+        <View className="flex flex-col">
+          <Text className={`text-${fontSizeData}xl text-black`}>
+            {t("Credential Type:\n")}
+            {item.credentialType}
+          </Text>
+        </View>
+        <View className="flex flex-col">
+          <Text className={`text-${fontSizeData}xl text-black`}>
+            {t("Start date:\n")}
+            {item.start_date.split("T")[0]}
+          </Text>
+        </View>
+        <View className="flex flex-col">
+          <Text className={`text-${fontSizeData}xl text-black`}>
+            {t("End date:\n")}
+            {item.end_date.split("T")[0]}
+          </Text>
+        </View>
+      </View>
       <View className="absolute bottom-0 right-0">
         <Text className="text-black opacity-20">{item.UUID}</Text>
       </View>
-      <MoreInfo
-        isHintVisible={isHintVisible}
-        setIsHintVisible={setIsHintVisible}
-        start_date={item.start_date.split("T")[0]}
-        end_date={item.end_date.split("T")[0]}
-      />
     </View>
   );
 };
