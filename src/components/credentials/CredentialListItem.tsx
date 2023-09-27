b4eb3d4e9d663e25fc712b69877233740f1e86d4
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-09-27 11:01:50 +0800

    add borderPosition props

--- src/components/credentials/CredentialListItem.tsx
+++ src/components/credentials/CredentialListItem.tsx
@@ -7,31 +7,37 @@ import { itemProps } from "~functions/api/credential/getCredentialList";
 
 type CredentialListItemProps = {
   item: itemProps;
+  borderPosition?: string;
 };
 
-const CredentialListItem: React.FC<CredentialListItemProps> = ({ item }) => {
+const CredentialListItem: React.FC<CredentialListItemProps> = ({
+  item,
+  borderPosition = "t",
+}) => {
   const { t } = useTranslation();
 
   const fontSizeData = useAtomValue(fontSizeAtom);
 
   return (
-    <View className="relative flex flex-row items-center border-t px-2 py-6">
+    <View
+      className={`relative flex flex-row items-center border-${borderPosition} px-2 py-6`}
+    >
       <View className="flex flex-row justify-between mb-4 w-full">
         <View className="flex flex-col">
           <Text className={`text-${fontSizeData}xl text-black`}>
-            {t("Credential Type:\n")}
+            {t("Credential Type")}:{"\n"}
             {item.credentialType}
           </Text>
         </View>
         <View className="flex flex-col">
           <Text className={`text-${fontSizeData}xl text-black`}>
-            {t("Start date:\n")}
+            {t("Start date")}:{"\n"}
             {item.start_date.split("T")[0]}
           </Text>
         </View>
         <View className="flex flex-col">
           <Text className={`text-${fontSizeData}xl text-black`}>
-            {t("End date:\n")}
+            {t("End date")}:{"\n"}
             {item.end_date.split("T")[0]}
           </Text>
         </View>
