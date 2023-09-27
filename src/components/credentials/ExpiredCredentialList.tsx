fd5d51a0403e1d05dc7a9147dc1836c09d0297e2
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-09-27 11:02:15 +0800

    import CredentialListItem

--- src/components/credentials/ExpiredCredentialList.tsx
+++ src/components/credentials/ExpiredCredentialList.tsx
@@ -1,16 +1,13 @@
-import { View, Text } from "react-native-ui-lib";
+import { View } from "react-native-ui-lib";
 import { useAtomValue } from "jotai";
 import { FlatList } from "react-native";
-import { useTranslation } from "react-i18next";
 
+import CredentialListItem from "./CredentialListItem";
 import expiredCredentialsAtom from "~atoms/expiredCredentials";
-import { fontSizeAtom } from "~atoms/fontSize";
 
 const ExpiredCredentialsList: React.FC = () => {
   const expiredCredentialsData = useAtomValue(expiredCredentialsAtom);
   console.log(expiredCredentialsData);
-  const fontSizeData = useAtomValue(fontSizeAtom);
-  const { t } = useTranslation();
 
   return (
     <View>
@@ -18,17 +15,8 @@ const ExpiredCredentialsList: React.FC = () => {
         className="h-full"
         data={expiredCredentialsData}
         renderItem={({ item }) => (
-          <View key={item.UUID}>
-            <View className="px-4 py-2">
-              <Text textColor className={`text-${fontSizeData + 1}xl`}>
-                {item.UUID}
-                {"\n"}
-                {t(`${item.issuer}`)}
-                {"\n"}
-                {t(`${item.credentialType}`)}
-              </Text>
-            </View>
-            <View bg-textColor className="h-[1px]"></View>
+          <View>
+            <CredentialListItem item={item} borderPosition="b" />
           </View>
         )}
         keyExtractor={(item) => item.UUID}
